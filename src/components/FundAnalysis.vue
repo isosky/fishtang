31e58f2ef837d87271efc1558e463e9359b45aa0
isowang <isowang@126.com>
2023-04-02 11:43:46 +0800

    add:左侧完成

--- src/components/FundAnalysis.vue
+++ src/components/FundAnalysis.vue
@@ -18,7 +18,7 @@
               :value="item.value"
             >
             </el-option> </el-select></el-col
-        ><el-col :span="3" :offset="1">
+        ><el-col :span="3">
           <el-date-picker
             v-model="date_selected"
             type="month"
@@ -29,7 +29,7 @@
           >
           </el-date-picker
         ></el-col>
-        <el-col :span="4" :offset="1">
+        <el-col :span="4">
           <el-select
             v-model="funder_selected"
             @change="getcalendar"
@@ -47,12 +47,21 @@
             </el-option> </el-select></el-col
         ><el-col :span="8" :offset="1">
           <el-button-group>
-            <el-button type="primary" icon="el-icon-arrow-left"></el-button>
-            <el-button type="primary" icon="el-icon-arrow-right"></el-button>
-            <el-button type="primary" icon="el-icon-refresh"></el-button>
             <el-button
               type="primary"
-              icon="el-icon-plus"
+              @click="lastmonth"
+              icon="el-icon-arrow-left"
+            ></el-button>
+            <el-button
+              type="primary"
+              @click="nextmonth"
+              icon="el-icon-arrow-right"
+            ></el-button>
+            <el-button type="primary" icon="el-icon-refresh"></el-button>
+            <el-button type="primary" icon="el-icon-plus"></el-button>
+            <el-button
+              type="primary"
+              icon="el-icon-coin"
             ></el-button></el-button-group
         ></el-col>
       </el-row>
@@ -63,18 +72,36 @@
               group-separator=","
               :precision="2"
               :value="sform.v1"
+              style="
+                height: 100px;
+                margin-top: 40px;
+                font-size: 30px;
+                text-align: center;
+              "
               title="历史收益"
             ></el-statistic>
             <el-statistic
               group-separator=","
               :precision="2"
               :value="sform.v2"
+              style="
+                height: 100px;
+                margin-top: 40px;
+                font-size: 30px;
+                text-align: center;
+              "
               title="持有收益"
             ></el-statistic>
             <el-statistic
               group-separator=","
               :precision="2"
               :value="sform.v3"
+              style="
+                height: 100px;
+                margin-top: 40px;
+                font-size: 30px;
+                text-align: center;
+              "
               title="持有收益率"
             ></el-statistic>
           </div> </el-col
@@ -86,8 +113,58 @@
         <div id="div_one_total" style="height: 450px"></div>
       </el-row>
     </el-col>
-    <el-col :span="6">234</el-col>
-    <el-col :span="6">456</el-col>
+    <el-col :span="12">
+      <el-row>
+        <el-table
+          :data="reviewtabledata"
+          show-summary
+          style="width: 100%"
+          height="700"
+          :cell-style="pricestyle"
+          ref="reviewtabledata"
+        >
+          <el-table-column
+            prop="fund_review_time"
+            label="时间"
+            width="120"
+          ></el-table-column>
+          <el-table-column prop="apps" label="来源" width="80">
+          </el-table-column>
+          <el-table-column prop="funder_name" label="博主名称" width="120">
+          </el-table-column>
+          <el-table-column
+            prop="fund_review_attitude"
+            label="走势预期"
+            width="60"
+          ></el-table-column>
+          <el-table-column
+            prop="fund_review"
+            label="点评"
+            width="300"
+          ></el-table-column>
+          <el-table-column prop="isfirm" label="实盘" width="80">
+          </el-table-column>
+          <el-table-column
+            prop="earn_percent"
+            sortable
+            label="收益率"
+            width="100"
+          >
+          </el-table-column>
+
+          <el-table-column label="操作" width="80">
+            <template slot-scope="scope">
+              <el-button
+                @click="diashowreview(scope.row)"
+                type="text"
+                size="small"
+                >复盘</el-button
+              >
+            </template>
+          </el-table-column>
+        </el-table>
+      </el-row>
+    </el-col>
   </div>
 </template>
   
@@ -97,6 +174,7 @@ var echarts = require("echarts");
 export default {
   data() {
     return {
+      reviewtabledata: [],
       fund_had_option: [],
       fund_had_list: "",
       fund_had_code_selected: "",
@@ -104,6 +182,7 @@ export default {
       funder_option: [],
       funder_selected: "",
       date_selected: "",
+      calendar_click: "",
       sform: {
         v1: 123.11,
         v2: 123.22,
@@ -111,10 +190,16 @@ export default {
       },
       calendar_chart: "",
       calendar_option: {
-        grid: {
-          left: "0",
-          right: "0",
+        // grid: {
+        //   left: "0",
+        //   right: "0",
+        // },
+        tooltip: {
+          formatter: function (params) {
+            return "收益率: " + params.value[1].toFixed(2);
+          },
         },
+        // TODO 颜色这块还得研究研究
         visualMap: {
           show: false,
           splitNumber: 10,
@@ -134,6 +219,9 @@ export default {
               "#ff9494",
               "#ff0000",
             ],
+          },
+          outOfRange: {
+            color: ["green", "red"],
           },
         },
         calendar: {
@@ -162,7 +250,6 @@ export default {
               show: true,
               formatter: function (params) {
                 var d = echarts.number.parseDate(params.value[0]);
-                console.log(params.value[2]);
                 return d.getDate() + "\n\n" + params.value[1] + "\n\n";
               },
               color: "#000",
@@ -238,6 +325,7 @@ export default {
     };
   },
   mounted: function () {
+    let that = this;
     this.calendar_chart = echarts.init(
       document.getElementById("calendar_div"),
       "white",
@@ -245,6 +333,11 @@ export default {
         renderer: "canvas",
       }
     );
+    this.calendar_chart.on("click", function (params) {
+      console.log(params.data[0]);
+      that.calendar_click = params.data[0];
+      that.getreviewtabledata();
+    });
     this.one_total_chart = echarts.init(
       document.getElementById("div_one_total"),
       "white",
@@ -256,27 +349,47 @@ export default {
   },
   methods: {
     init: function () {
-      console.log("temp");
+      //   console.log("temp");
       this.gethadfund();
     },
+    getreviewtabledata: function () {
+      if (this.calendar_click == "") {
+        let y = new Date().getFullYear() + "-";
+        let m =
+          new Date().getMonth() + 1 < 10
+            ? "0" + (new Date().getMonth() + 1) + "-"
+            : new Date().getMonth() + 1 + "-";
+        let d =
+          new Date().getDate() < 10
+            ? "0" + new Date().getDate()
+            : new Date().getDate();
+        let currentDate = y + m + d;
+        this.calendar_click = currentDate;
+      }
+      axios
+        .post("/getreviewtabledata", {
+          fund_code: this.fund_had_code_selected,
+          fund_review_time: this.calendar_click,
+        })
+        .then((response) => {
+          console.log(response);
+          this.reviewtabledata = response.data;
+        });
+    },
     getcalendar: function () {
-      console.log(this.fund_had_code_selected);
-      console.log(this.date_selected);
       if (this.fund_had_code_selected != "" && this.date_selected != "") {
         console.log("我的某个基金");
-        // this.fund_had_name_selected =
-        //   this.fund_had_list[this.fund_had_code_selected];
         axios
           .post("/getfundcalendar", {
             fund_code: this.fund_had_code_selected,
           })
           .then((response) => {
-            console.log(response);
+            // console.log(response);
             this.calendar_option.series[0].data = response.data.bs_data;
             this.calendar_option.series[1].data = response.data.bs_data;
             this.calendar_option.series[2].data = response.data.data;
             this.calendar_option.calendar.range = this.date_selected;
-            console.log(this.calendar_option);
+            // console.log(this.calendar_option);
             this.calendar_chart.setOption(this.calendar_option);
           });
 
@@ -299,7 +412,53 @@ export default {
             : new Date().getMonth() + 1;
         this.date_selected = y + m;
         this.getcalendar();
+        this.getreviewtabledata();
       });
+    },
+    lastmonth: function () {
+      let arr = this.date_selected.split("-");
+      let year = arr[0]; //获取当前日期的年份
+      let month = arr[1]; //获取当前日期的月份
+
+      let year2 = year;
+      let month2 = parseInt(month) - 1;
+      if (month2 == 0) {
+        //1月的上一月是前一年的12月
+        year2 = parseInt(year2) - 1;
+        month2 = 12;
+      }
+
+      if (month2 < 10) {
+        //10月之前都需要补0
+        month2 = "0" + month2;
+      }
+      let preMonth = year2 + "-" + month2;
+      this.date_selected = preMonth;
+      this.getcalendar();
+    },
+    nextmonth: function () {
+      var arr = this.date_selected.split("-");
+      let year = arr[0]; //获取当前日期的年份
+      let month = arr[1]; //获取当前日期的月份
+
+      let year2 = year;
+      let month2 = parseInt(month) + 1;
+      if (month2 == 13) {
+        //12月的下月是下年的1月
+        year2 = parseInt(year2) + 1;
+        month2 = 1;
+      }
+      if (month2 < 10) {
+        //10月之前都需要补0
+        month2 = "0" + month2;
+      }
+
+      let nextMonth = year2 + "-" + month2;
+      this.date_selected = nextMonth;
+      this.getcalendar();
+    },
+    pricestyle: function ({ row, column, rowIndex, columnIndex }) {
+      // console.log(columnIndex);
     },
     setonetotalchart: function () {
       axios
